9d8dc9392efce824219758fc7b7f4199d77b12d5
taleksey <alterehov@gmail.com>
2024-10-29 10:13:03 +0200

    init

---
+++ src/test/java/org/aleksey/springnew/SpringnewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aleksey.springnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

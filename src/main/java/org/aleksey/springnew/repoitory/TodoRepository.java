7c07754ec773f747d350cbe3727ea2d69437a690
taleksey <alterehov@gmail.com>
2024-11-08 10:05:16 +0200

    Spring add

--- src/main/java/org/aleksey/springnew/repoitory/TodoRepository.java
+++ src/main/java/org/aleksey/springnew/repoitory/TodoRepository.java
@@ -1,9 +1,7 @@
 package org.aleksey.springnew.repoitory;
 
-import jakarta.transaction.Transactional;
 import org.aleksey.springnew.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
